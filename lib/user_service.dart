026d838a7bc4e2687721ee52ff45cf1c30f17f77
matthaeusheer <matthaeusheer@gmail.com>
2023-10-10 18:38:46 +0200

    adapted to userId on flight and uuid

---
+++ lib/user_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'model.dart';
+
+const BASE_URL = 'http://10.0.2.2:8080';
+// const BASE_URL = 'https://floaty-backend-floaty-backend.azuremicroservices.io';
+
+Future<User> fetchUserById(String userId) async {
+  final String apiUrl = '$BASE_URL/users/$userId';
+
+  final response = await http.get(Uri.parse(apiUrl));
+
+  if (response.statusCode == 200) {
+    return User.fromJson(json.decode(response.body));
+  } else {
+    // TODO (Matthäus): This is bad.
+    throw Exception('Failed to load user');
+  }
+}
